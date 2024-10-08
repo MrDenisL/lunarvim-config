c55c8e8f2128fbf0cc704b37e6860df670231c36
denisl <denisl@gilat.com>
2024-10-08 11:38:16 +0300

    adding relative line numbers + clear search pattern shortcut

--- config.lua
+++ config.lua
@@ -36,3 +36,8 @@ lvim.keys.visual_mode["<S-Tab>"] = "<gv"
 vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
 vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true -- Convert tabs to spaces
+-- relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- clean search pattern 
+lvim.keys.normal_mode["<leader>n"] = ":nohlsearch<CR>"
